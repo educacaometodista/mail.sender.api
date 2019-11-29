a52fab241eae34deb5989ea399eafc66990482bb
LeuAlmeida <leo@webid.net.br>
2019-11-29 18:46:00 -0300

    Create sender route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import SenderController from './app/controllers/SenderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,5 +16,7 @@ routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 routes.get('/users', UserController.index);
+
+routes.post('/senders', SenderController.store);
 
 export default routes;
