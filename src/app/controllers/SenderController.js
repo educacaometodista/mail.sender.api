3a1e4ca23ef78f008a3356aee8dea18e9bc66e8a
LeuAlmeida <leo@webid.net.br>
2019-11-29 18:56:54 -0300

    Improving sender controller

--- src/app/controllers/SenderController.js
+++ src/app/controllers/SenderController.js
@@ -1,6 +1,33 @@
+import * as Yup from 'yup';
+import Sender from '../models/Sender';
+
 class SenderController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Falha na validação' });
+    }
+
     const { name, email } = req.body;
+
+    const senderExists = await Sender.findOne({
+      where: { email },
+    });
+
+    if (senderExists) {
+      return res.status(401).json({ error: 'Este remetente já existe.' });
+    }
+
+    await Sender.create({
+      name,
+      email,
+    });
 
     return res.json({
       name,
