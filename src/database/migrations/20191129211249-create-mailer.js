6ce3cb9eb8c41d609a6c24839103bf2457b732b7
LeuAlmeida <leo@webid.net.br>
2019-11-29 18:40:49 -0300

    Improving Mailer migration

--- src/database/migrations/20191129211249-create-mailer.js
+++ src/database/migrations/20191129211249-create-mailer.js
@@ -7,6 +7,28 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
       },
+      subject: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sender_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'senders', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
     });
   },
 
