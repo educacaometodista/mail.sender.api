eee35b7fa4a2a5311fb0db2c6e5802eb5c7f80cd
LeuAlmeida <leo@webid.net.br>
2019-11-29 15:37:35 -0300

    Improving UserController (store method)

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,30 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Falha na validação dos campos.' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res
+        .status(401)
+        .json({ error: 'Um usuário com este e-mail já existe.' });
+    }
+
     const { id, name, email } = await User.create(req.body);
 
     return res.json({
