0c17a5f60358723b2fd04d72311ffd0f51662219
LeuAlmeida <leo@webid.net.br>
2019-11-29 16:40:57 -0300

    Incrementing avatar_url into Users

--- src/database/migrations/20191129173844-create-admins.js
+++ src/database/migrations/20191129173844-create-admins.js
@@ -19,6 +19,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      avatar_url: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
--- src/database/seeds/20191129192439-create-admins.js
+++ src/database/seeds/20191129192439-create-admins.js
@@ -10,6 +10,8 @@ module.exports = {
           name: 'Leonardo Almeida',
           email: process.env.LEONARDO_MAIL,
           password_hash: bcrypt.hashSync(process.env.LEONARDO_PASS, 8),
+          avatar_url:
+            'https://avatars3.githubusercontent.com/u/42948574?s=460&v=4',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -18,6 +20,8 @@ module.exports = {
           name: 'Erick Firmo',
           email: process.env.ERICK_MAIL,
           password_hash: bcrypt.hashSync(process.env.ERICK_PASS, 8),
+          avatar_url:
+            'https://avatars3.githubusercontent.com/u/34639603?s=460&v=4',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -26,6 +30,8 @@ module.exports = {
           name: 'Thiago Tamosauskas',
           email: process.env.THIAGO_MAIL,
           password_hash: bcrypt.hashSync(process.env.THIAGO_PASS, 8),
+          avatar_id:
+            'https://scontent.fcgh18-1.fna.fbcdn.net/v/t1.15752-9/74674923_3214558651952697_1563643398852706304_n.jpg?_nc_cat=108&_nc_ohc=cTaAYB_1Q44AQk9GzDMO5_fhq-cOHKqw8Eq_JK9GExzilaELZXYyp959g&_nc_ht=scontent.fcgh18-1.fna&oh=08c8cd3b06327b208dd958a50a1189ec&oe=5E8A835F',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -34,6 +40,8 @@ module.exports = {
           name: 'Fernando Zancopé',
           email: process.env.FERNANDO_MAIL,
           password_hash: bcrypt.hashSync(process.env.FERNANDO_PASS, 8),
+          avatar_url:
+            'https://avatars0.githubusercontent.com/u/53548067?s=460&v=4',
           created_at: new Date(),
           updated_at: new Date(),
         },
