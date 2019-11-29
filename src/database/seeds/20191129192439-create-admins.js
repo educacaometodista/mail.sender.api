577c2fdd8b5b5172667366a743e3c6555b7583e5
LeuAlmeida <leo@webid.net.br>
2019-11-29 16:32:44 -0300

    Update admins seeder

--- src/database/seeds/20191129192439-create-admins.js
+++ src/database/seeds/20191129192439-create-admins.js
@@ -1,11 +1,41 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: queryInterface => {
     return queryInterface.bulkInsert(
       'users',
       [
         {
-          name: 'John Doe',
-          isBetaMember: false,
+          id: 1,
+          name: 'Leonardo Almeida',
+          email: process.env.LEONARDO_MAIL,
+          password_hash: bcrypt.hashSync(process.env.LEONARDO_PASS, 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 2,
+          name: 'Erick Firmo',
+          email: process.env.ERICK_MAIL,
+          password_hash: bcrypt.hashSync(process.env.ERICK_PASS, 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 3,
+          name: 'Thiago Tamosauskas',
+          email: process.env.THIAGO_MAIL,
+          password_hash: bcrypt.hashSync(process.env.THIAGO_PASS, 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 4,
+          name: 'Fernando Zancopé',
+          email: process.env.FERNANDO_MAIL,
+          password_hash: bcrypt.hashSync(process.env.FERNANDO_PASS, 8),
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
