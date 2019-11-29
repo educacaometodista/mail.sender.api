0f7283e16aa4b1db19c05ff41aff9af79bc35ba9
LeuAlmeida <leo@webid.net.br>
2019-11-29 17:42:08 -0300

    Improving User Controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,7 +5,9 @@ class UserController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
       password: Yup.string()
         .required()
         .min(6),
@@ -32,6 +34,10 @@ class UserController {
       name,
       email,
     });
+  }
+
+  async update(req, res) {
+    return res.json();
   }
 }
 
