dc4b92a9114ca4cac55f03c17a3fd0a109e121e5
LeuAlmeida <leo@webid.net.br>
2019-11-29 18:46:20 -0300

    Including created and updated at fields

--- src/database/migrations/20191129211243-create-sender.js
+++ src/database/migrations/20191129211243-create-sender.js
@@ -15,6 +15,14 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
