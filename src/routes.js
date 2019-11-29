d58e068b761cd340f45c29bb52b589e29675a753
LeuAlmeida <leo@webid.net.br>
2019-11-29 15:17:15 -0300

    Incrementing User post route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ Teste: true }));
+routes.post('/users', UserController.store);
 
 export default routes;
