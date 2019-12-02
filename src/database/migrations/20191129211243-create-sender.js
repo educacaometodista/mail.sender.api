5dce4cb010ce6a794eb74df894d3ec176ae1ffd7
LeuAlmeida <leo@webid.net.br>
2019-12-02 14:36:05 -0300

    Create sender seeder

--- src/database/migrations/20191129211243-create-sender.js
+++ src/database/migrations/20191129211243-create-sender.js
@@ -19,6 +19,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      top: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
