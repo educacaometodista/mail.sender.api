2ee7b004cc2efe2582d522b13acf99a88a3ebc06
LeuAlmeida <leo@webid.net.br>
2019-12-02 19:06:05 -0300

    Update sender seeder

--- src/database/seeds/20191202173543-create-senders.js
+++ src/database/seeds/20191202173543-create-senders.js
@@ -25,6 +25,50 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date(),
         },
+        {
+          id: 3,
+          name: 'Faculdade Metodista Centenário',
+          email: 'informes.centenario@centenario.metodista.br',
+          initials: 'fames',
+          top:
+            'http://metodistacentenario.com.br/msg/comunicados/2019/novo/template/topo-novo-fames.jpg',
+          color: '#246834',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 4,
+          name: 'Centro Universitário Metodista Izabela Hendrix',
+          email: 'informes.imih@izabelahendrix.metodista.br',
+          initials: 'izabela',
+          top:
+            'http://izabelahendrix.edu.br/msg/comunicados/2019/novo/template/topo-novo-izabela.jpg',
+          color: '#a32d27',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 5,
+          name: 'Centro Universitário Metodista IPA',
+          email: 'comunicados.ipa@ipametodista.edu.br',
+          initials: 'ipa',
+          top:
+            'http://ipametodista.edu.br/msg/comunicados/2019/novo/template/topo-novo-ipa.jpg',
+          color: '#b8974a',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 6,
+          name: 'Faculdade Metodista Granbery',
+          email: 'informes.granbery@granbery.metodista.br',
+          initials: 'granbery',
+          top:
+            'http://granbery.edu.br/msg/comunicados/2019/novo/template/topo-novo-granbery.jpg',
+          color: '#242855',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
       ],
       {}
     );
