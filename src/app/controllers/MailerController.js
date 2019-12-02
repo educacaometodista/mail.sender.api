fb57aeaa4155847a80cfef07a66b5e107dbbbb68
LeuAlmeida <leo@webid.net.br>
2019-12-02 12:17:11 -0300

    Improving MailerController

--- src/app/controllers/MailerController.js
+++ src/app/controllers/MailerController.js
@@ -1,6 +1,26 @@
+import Mailer from '../models/Mailer';
+
 class MailerController {
   async store(req, res) {
-    return res.json();
+    const author_id = req.userId;
+
+    const { id, sender_id, subject, body } = req.body;
+
+    await Mailer.create({
+      id,
+      sender_id,
+      subject,
+      body,
+      author_id,
+    });
+
+    return res.json({
+      id,
+      sender_id,
+      subject,
+      body,
+      author_id,
+    });
   }
 }
 
