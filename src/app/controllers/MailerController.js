64f8e393cf66848cfed7db25209f8aad554502fd
LeuAlmeida <leo@webid.net.br>
2019-12-02 12:24:27 -0300

    Improving Mailer Controller

--- src/app/controllers/MailerController.js
+++ src/app/controllers/MailerController.js
@@ -1,10 +1,32 @@
+import * as Yup from 'yup';
 import Mailer from '../models/Mailer';
+import Sender from '../models/Sender';
 
 class MailerController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      sender_id: Yup.number().required(),
+      subject: Yup.string()
+        .required()
+        .min(16),
+      body: Yup.string()
+        .required()
+        .max(9999),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Erro na validação.' });
+    }
+
     const author_id = req.userId;
 
     const { id, sender_id, subject, body } = req.body;
+
+    const senderExists = await Sender.findByPk(sender_id);
+
+    if (!senderExists) {
+      return res.status(400).json({ error: 'Remetente não encontrado' });
+    }
 
     await Mailer.create({
       id,
