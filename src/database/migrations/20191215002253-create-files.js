0021331f90a0f9f1cf55ba49c605ff32b53a78fb
Léu Almeida <leo@webid.net.br>
2019-12-15 00:26:51 -0300

    chore: Including declaration field in File migration

--- src/database/migrations/20191215002253-create-files.js
+++ src/database/migrations/20191215002253-create-files.js
@@ -16,6 +16,10 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      declaration: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
